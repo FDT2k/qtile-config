7a78e84c8b18b51c35e42d77aab23cb364287e29
Fabien Karsegard <fabien@karsegard.ch>
2020-10-01 12:10:38 +0200

    Update barrier.sgc, autostart.sh, and 2 more files...

--- autostart.sh
+++ autostart.sh
@@ -5,6 +5,5 @@ thunderbird &
 bin/clickup &
 
 sleep 1
-bash bin/last-layout >> ~/.config/qtile/layout.log
 
 feh --bg-fill ~/background.jpg &
---
+++ bin/screenlayout.d/alien-thin.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --off --output DP1 --off --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate left --output HDMI2 --off --output VIRTUAL1 --off --output DP-1-2 --off --output DP-1-3 --off --output HDMI-1-3 --off --output DVI-I-1-1 --off
