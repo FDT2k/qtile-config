aad14fbe781fe7ab8fe9a15f54bfb9952e9739af
Fabien Karsegard <fabien@karsegard.ch>
2022-01-09 02:26:21 +0100

    Update fdt2k.config.py

--- fdt2k.config.py
+++ fdt2k.config.py
@@ -31,7 +31,7 @@ from libqtile.lazy import lazy
 from libqtile import layout, bar, widget, hook
 from libqtile.log_utils import logger
 from typing import List  # noqa: F401
-
+from rofi import Rofi
 #from libqtile import xcbq
 #xcbq.keysyms["XF86AudioRaiseVolume"] = 0x1008ff13
 #xcbq.keysyms["XF86AudioLowerVolume"] = 0x1008ff11
@@ -44,6 +44,38 @@ mod = "mod4"
 alt = "mod1"
 ctrl = "control"
 shft =  "shift"
+
+rofi_l = Rofi(rofi_args=['-theme', '~/.config/rofi/left_toolbar.rasi'])
+rofi_r = Rofi(rofi_args=['-theme', '~/.config/rofi/right_toolbar.rasi'])
+
+
+def get_net_dev():
+    get_dev = "ip addr show | awk '/inet.*brd/{print $NF; exit}'"
+    ps = subprocess.Popen(get_dev,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    output = ps.communicate()[0].decode('ascii').strip()
+    return(output)
+
+
+internet = ' Yei Internet is working!'
+def get_public_ip():
+    try:
+        raw = requests.get('https://api.duckduckgo.com/?q=ip&format=json')
+        answer = raw.json()["Answer"].split()[4]
+    except Exception as e:
+        return "0.0.0.0"
+    else:
+        return answer
+       
+public_ip = get_public_ip()
+
+if public_ip.startswith('0'):
+    internet = "OMG You Have No Internet"
+    
+wifi = get_net_dev()
+if wifi.startswith('w'):
+    wifi_icon=' '
+else:
+    wifi_icon=' '
 
 
 @hook.subscribe.screen_change
@@ -95,6 +127,34 @@ def init_colors():
 
 color = init_colors()
 
+
+def network_widget(qtile):
+    get_ssid = "iwgetid -r"
+    pos = subprocess.Popen(get_ssid,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    ssid = pos.communicate()[0].decode('ascii').strip()
+    get_status = "nmcli radio wifi"
+    ps = subprocess.Popen(get_status,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    status = ps.communicate()[0].decode('ascii').strip()
+    if status == 'enabled':
+        connected = ' Turn Wifi Off'
+        active = "off"
+    else:
+        connected = ' Turn Wifi On'
+        active= "on"
+    options = [connected,' Bandwith Monitor (CLI)', ' Network Manager (CLI)', ' Network Manager (GUI)']
+    index, key = rofi_r.select(wifi_icon + internet, options)
+    if key == -1:
+        rofi_r.close()
+    else:
+        if index ==0:
+            subprocess.run("nmcli radio wifi " + active, shell=True)
+        elif index==1:
+            qtile.cmd_spawn(term + ' -e bmon')
+        elif index==2:
+            qtile.cmd_spawn(term + ' -e nmtui')
+        else:
+            qtile.cmd_spawn('nm-connection-editor')
+        
 class command:
     #terminal = get_alternatives(['terminator', 'gnome-terminal', 'xterm'])
     autostart = os.path.join(os.path.dirname(__file__), 'bin/autostart')
@@ -123,6 +183,7 @@ class command:
     sound = os.path.join(os.path.dirname(__file__), 'bin/pulsaudio/sound-output.sh')
     theme = os.path.join(os.path.dirname(__file__), 'bin/theme/pick')
     screen_layout = os.path.join(os.path.dirname(__file__), 'bin/run.sh screenlayout.d "Monitor Layout"')
+    
 
 class theme:
     bg = "#283033"
@@ -247,7 +308,7 @@ keys = [
 
     #app shortcuts
 
-    Key([mod, alt], "n", lazy.spawn("networkmanager_dmenu")),
+    #Key([mod, alt], "n", lazy.spawn("networkmanager_dmenu")),
     Key([mod, alt], "q", lazy.spawn(command.browser)),
     Key([mod, alt], "a", lazy.spawn(command.app_menu)),
     Key([mod], "r", lazy.spawn(command.run)),
@@ -263,7 +324,7 @@ keys = [
     Key([mod, alt], "2", lazy.spawn(command.middle_screen_brightness)),
     Key([mod, alt], "3", lazy.spawn(command.right_screen_brightness)),
     Key([mod, alt], "1", lazy.spawn(command.left_screen_brightness)),
-
+    Key([mod, alt], "n", lazy.function(network_widget)),
     
     # launch graphic layout
 #    Key([mod, alt], "y", lazy.function(set_vertical_monitor_layout)),
@@ -462,6 +523,7 @@ screens = [
                     border_width=2,
                     line_width=1
                 ),
+                widget.Battery(),
                  widget.TextBox(
                     font="Arial",
                     foreground="#CACACA",
